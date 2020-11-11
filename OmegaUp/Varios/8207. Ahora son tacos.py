c72a7208f07f51ea73086b68f28c929e7ea43bd2
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2020-11-11 09:00:44 -0600

    Add files via upload

---
+++ OmegaUp/Varios/8207. Ahora son tacos.py
@@ -0,0 +1,9 @@
+a1 = int(raw_input())
+a2 = int(raw_input())
+a3 = int(raw_input())
+a4 = int(raw_input())
+a5 = int(raw_input())
+a6 = int(raw_input())
+
+suma = (a1*a4)+(a2*a5)+(a3*a6)
+print suma
