2fb548f13993388476ef8592081749a33db829ec
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2020-11-27 10:26:28 -0600

    Add files via upload

---
+++ OmegaUp/Varios/8167. Contando los medicamentos.py
@@ -0,0 +1,10 @@
+edad = int(raw_input())
+if (edad <=10) :
+	medicamentos = 2
+elif (edad <=17) :
+	medicamentos = 5
+elif (edad <=60) :
+	medicamentos = 8
+else:
+	medicamentos = 10
+print medicamentos
