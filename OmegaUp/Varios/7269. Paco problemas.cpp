fc342365ce344ffc1227ea27c751384cc4d635fd
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-05-25 07:08:23 -0500

    Add files via upload

---
+++ OmegaUp/Varios/7269. Paco problemas.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+string cosa;
+unsigned long long int num,res;
+int main() {
+    cin >> num;
+    res = (num*(num+1))/2;
+    cout << res;
+    return 0;
+}
