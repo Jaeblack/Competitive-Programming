db125857794a09f02242f887a3000941b4c0f1ac
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-05-25 07:07:38 -0500

    Add files via upload

---
+++ OmegaUp/Varios/7294. ¿Cuántos valen 7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+long long int num,c;
+int main()
+{
+    cin >> num;
+    if (num == 7) c++;
+    cin >> num;
+    if (num == 7) c++;
+    cout << c;
+    return 0;
+}
