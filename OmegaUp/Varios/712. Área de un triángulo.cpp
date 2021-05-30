6a85440203aa57ccebf2ba022114ce66c2eb1cb2
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-05-30 15:45:32 -0500

    Add files via upload

---
+++ OmegaUp/Varios/712. Área de un triángulo.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double a,b,c;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	
+	cin >> a >> b ;
+	c = (a*b)/2;
+	cout << setprecision(2) << fixed << c;
+	
+	return 0;
+}
