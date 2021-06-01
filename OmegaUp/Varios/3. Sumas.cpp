d07c2d8715f4b21b42d5b6c1b97b89a92d08d90f
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-06-01 09:32:13 -0500

    Add files via upload

---
+++ OmegaUp/Varios/3. Sumas.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int X,Y;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> X >> Y;
+    X+=Y;
+    cout << X;
+    return 0;
+}
