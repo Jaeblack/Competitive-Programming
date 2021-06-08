5b38e0aa1e16abe6a5c4fbf023cbf223d850bbf2
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-06-07 19:20:49 -0500

    Add files via upload

---
+++ OmegaUp/Varios/4252. Bisiesto UP.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int N,M,i;
+char chairo;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    
+        cin >> N;
+    if((N%4==0 && N%100!=0) || (N%100 == 0 && N%400 == 0)) {
+        cout << "Es bisiesto" << '\n';
+    } else {
+        cout << "No es bisiesto" << '\n';
+    }
+    
+    
+
+    return 0;
+}
