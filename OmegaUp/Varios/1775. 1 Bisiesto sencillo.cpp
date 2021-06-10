422d9bdff3cab1a965e5a3805cbce5bd7dfb3cc5
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-06-09 19:43:49 -0500

    Add files via upload

---
+++ OmegaUp/Varios/1775. 1 Bisiesto sencillo.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int N,M,i;
+char chairo;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> N;
+    if((N%4==0 && N%100!=0) || (N%100 == 0 && N%400 == 0)) {
+        cout << 'S';
+    } else {
+        cout << 'N';
+    }
+
+    return 0;
+}
