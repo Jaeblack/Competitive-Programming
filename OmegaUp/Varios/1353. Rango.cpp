780d6f11c20acb0232f4a2276dab2438bbba73e9
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-06-20 17:25:30 -0500

    Add files via upload

---
+++ OmegaUp/Varios/1353. Rango.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int n,m;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    for(int i =n; i<=m;i++){
+        cout << i << " ";
+    }
+    return 0;
+}
