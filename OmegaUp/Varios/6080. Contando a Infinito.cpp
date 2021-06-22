fbcc7349fe31eee0a731ce311e8656249fd8f19a
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-06-22 07:46:10 -0500

    Add files via upload

---
+++ OmegaUp/Varios/6080. Contando a Infinito.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int i,n;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    for(i=1;i<=n;i++){
+        cout << i << '\n';
+    }
+    return 0;
+}
