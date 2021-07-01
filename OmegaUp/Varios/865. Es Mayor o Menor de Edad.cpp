08fcbb6ef9216be872dc7c88f2ad544e7e1f1bd2
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-07-01 12:18:35 -0500

    Add files via upload

---
+++ OmegaUp/Varios/865. Es Mayor o Menor de Edad.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int a,b,c,d,r;
+int N;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> a;
+    if(a >= 18){
+        cout << "Es Mayor";
+    } else {
+        cout << "Es Menor";
+    }
+       
+    return 0;
+}
