cf4718dd4a32a0ca8b89736c9a86913ffea3a8d9
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-07-06 11:59:13 -0500

    Add files via upload

---
+++ OmegaUp/Varios/6115. A + B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int a,b,c,d,r,pos;
+int N;
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> a >> b;
+    cout << (a+b);
+
+    return 0;
+}
