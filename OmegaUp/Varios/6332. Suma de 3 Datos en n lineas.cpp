52f7364f67a161342b9b6390b51a80b26e745932
Jaeblack <37078683+Jaeblack@users.noreply.github.com>
2021-07-10 12:11:24 -0500

    Add files via upload

---
+++ OmegaUp/Varios/6332. Suma de 3 Datos en n lineas.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long int a,b,c,r;
+int N;
+int main()
+{
+    cin >> N;
+
+    for(int i=0;i<N;i++){
+        cin >> a >> b >> c;
+        cout << (a+b+c) << '\n';
+    }
+
+    return 0;
+}
